107c0f5f3cc78c04e9f27b7a50ab34f6975d5e3a
Gal Noy <gal@galnoy.com>
2019-03-10 00:47:28 +0200

    small improvments

--- packages/server-common/src/factory/mongodb.js
+++ packages/server-common/src/factory/mongodb.js
@@ -1,16 +1,13 @@
-const MongoClient = require('mongodb').MongoClient
-const ObjectId = require('mongodb').ObjectId
-
 /**
  *  @param {Object} options options object
+ *  @param {Object} options.mongodb mongodb object
  *  @param {string} options.mongoUri remote mongodb url - https://docs.mongodb.com/manual/reference/connection-string/
- *  @param {string} options.dbName Database name, default: 'testing'
  *  @param {Object} options.mongodbOptions options mongodb client - http://mongodb.github.io/node-mongodb-native/2.2/api/MongoClient.html
  *  @returns {Promise} - resolved to db
  */
 const create = ({
   mongoUri = 'mongodb://localhost:27017/testing',
-  dbName = 'testing',
+  mongodb: { MongoClient, ObjectId } = require('mongodb'),
   mongodbOptions = {}
 }) =>
   MongoClient.connect(mongoUri, {
@@ -18,6 +15,10 @@ const create = ({
     useNewUrlParser: true,
     ...mongodbOptions
   })
-    .then(client => client.db(dbName))
+    .then(client => ({
+      ObjectId,
+      db: client.db(),
+      close: () => client.close()
+    }))
 
-module.exports = { create, ObjectId }
+module.exports = { create }
